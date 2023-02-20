3b9ae26c4f3dfbb26f29537db9b1ada7e6fcd031
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-20 23:24:18 +0100

    schoolCatalog_initialization

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -4,4 +4,22 @@ class School {
     this._level = level;
     this._numberOfStudents = numberOfStudents
   }
+
+  get name() {
+    return this._name;
+  };
+
+  get level() {
+    return this._numberOfStudents;
+  };
+
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  };
+
+  set numberOfStudents() {
+    if (numberOfStudents === number) {
+    return numberOfStudents}else{
+      return "Invalid input: numberOfStudents must be set to a Number."}
+  }
 }
