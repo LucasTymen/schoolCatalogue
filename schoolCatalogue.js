626a45eb566c71467af059295ec9f50eea79e94a
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-20 23:35:24 +0100

    schoolCatalog_school

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -2,24 +2,26 @@ class School {
   constructor(name) {
     this._name = name;
     this._level = level;
-    this._numberOfStudents = numberOfStudents
+    this._numberOfStudents = numberOfStudents;
   }
 
   get name() {
     return this._name;
-  };
+  }
 
   get level() {
     return this._numberOfStudents;
-  };
+  }
 
   get numberOfStudents() {
     return this._numberOfStudents;
-  };
+  }
 
-  set numberOfStudents() {
-    if (numberOfStudents === number) {
-    return numberOfStudents}else{
-      return "Invalid input: numberOfStudents must be set to a Number."}
+  set numberOfStudents(newNumberOfStudents) {
+    if (this._numberOfStudents === "number") {
+      return this._numberOfStudents;
+    } else {
+      return "Invalid input: numberOfStudents must be set to a Number.";
+    }
   }
 }
