b7119b0796ece1aa1581a43f5b89638ba8e6492c
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-21 14:23:46 +0100

    hischool

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -17,11 +17,43 @@ class School {
     return this._numberOfStudents;
   }
 
-  set numberOfStudents(newNumberOfStudents) {
-    if (this._numberOfStudents === "number") {
-      return this._numberOfStudents;
+  set numberOfStudents(value) {
+    if (value.isNaN()) {
+      console.log("Invalid input: numberOfStudents must be set to a Number.");
     } else {
-      return "Invalid input: numberOfStudents must be set to a Number.";
+      this._numberOfStudents = value;
     }
+  }
+
+  quickFacts() {
+    console.log(
+      `${school.name} educates ${numberOfStudents} students at the ${level} school level`
+    );
+  }
+
+  static pickSubtituteTeacher(substituteTeachers) {
+    const randInt = Math.floor(Math.random() * substituteTeachers.length);
+    return substituteTeachers[randInt];
+  }
+}
+
+class PrimarySchool extends School {
+  constructor(name) {
+    super(name, "primary", numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+class HighSchool extends School{
+  constructor(name){
+    super(name, "HighSchool", numberOfStudents)
+    this._sportsTeam = sportsTeams;
+  }
+  get sportsTeams() {
+    return this._sportsTeams;
   }
 }
